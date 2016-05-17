ecc4525018a87bf26e7a895c9f8d5012d60ff9b7
Howard Chen <ibanezchen@gmail.com>
2016-05-17 21:57:13 +0800

    Disable _ENABLE_THREAD_SUPPORT_

--- platform/hyperc/timer.c
+++ platform/hyperc/timer.c
@@ -1,22 +1,21 @@
 #include "timer_interface.h"
 #include <hcos/tmr.h>
-
-extern unsigned _HZ;
+#include "plt.h"
 
 bool has_timer_expired(Timer* timer) {
-	return timer->expire > tmr_ticks;
+	return timer->expire < tmr_ticks;
 }
 
 void countdown_ms(Timer* timer, uint32_t timeout) {
-	timer->expire = tmr_ticks + (timeout*_HZ)/1000;
+	timer->expire = tmr_ticks + (timeout*PLT_HZ)/1000;
 }
 
 void countdown_sec(Timer* timer, uint32_t timeout) {
-	timer->expire = tmr_ticks + (timeout*_HZ);
+	timer->expire = tmr_ticks + (timeout*PLT_HZ);
 }
 
 uint32_t left_ms(Timer* timer) {
-	return ((timer->expire - tmr_ticks) * 1000)/_HZ;
+	return ((timer->expire - tmr_ticks) * 1000)/PLT_HZ;
 }
 
 void init_timer(Timer* timer) 
